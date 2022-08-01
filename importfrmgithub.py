6c3b29931fd427f2984ab4ba687d4c7e0b06cb6c
aritraghosh78 <110330265+aritraghosh78@users.noreply.github.com>
2022-08-01 15:37:26 +0530

    Add files via upload

---
+++ importfrmgithub.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+pd.read_csv(r"C:\Users\91967\Documents\Book1.csv")
+
+
+# In[3]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/Skyserver.csv")
+
+
+# In[ ]:
+
+
+
+
